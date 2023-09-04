35873199c31ab68a0dc31b11972236a1f3d2b363
Ashim Gautam Upadhaya <ashimupadhaya@Ashims-Air.hitronhub.home>
2023-09-03 20:34:31 -0500

    Email list functionality

--- monoscript/index.js
+++ monoscript/index.js
@@ -1,13 +1,44 @@
+const sql = require('mssql');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // Validate input
+    if (!req.body.name || !req.body.email){
+        context.res = {
+            status: 400,
+            body: "Name and email are required"
+        }
+    }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    const name = req.body.name;
+    const email = req.body.email;
+    const phoneNumber = req.body.phoneNumber || null;
+    const date = new Date();
+
+    try {
+        await sql.connect(process.env.dbConnectionString);
+
+        const existingUser = await sql.query`SELECT * FROM EmailList WHERE Email = ${email}`;
+        
+        if (existingUser?.recordset?.length > 0) {
+            context.res = {
+                status: 409,
+                body: "user with this email already exisits"
+            }
+            return;
+        }
+        const result = await sql.query`INSERT INTO EmailList (Name, Email, PhoneNumber, DateAdded) VALUES (${name}, ${email}, ${phoneNumber}, ${date})`;
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Data inserted with success"
+        };
+    }
+    catch (err)
+    {
+        context.log.error('SQL error: Error connecting to the db', err);
+        context.res = {
+            status: 500,
+            body: "Error connecting to the db"
+        }
+    }
 }
